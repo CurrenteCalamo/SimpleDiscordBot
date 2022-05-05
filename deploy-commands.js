d9086b2fcda5b8392ce4977f4d4ff486309d0811
AbIgneIgnem <pppp43777@gmail.com>
2022-05-05 15:56:10 +0300

    Initiall commit

---
+++ deploy-commands.js
@@ -0,0 +1,18 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const { REST } = require('@discordjs/rest');
+const { Routes } = require('discord-api-types/v9');
+const { clientId, token } = require('./config.json');
+
+const commands = [
+	new SlashCommandBuilder().setName('ping').setDescription('Replies with pong!'),
+	new SlashCommandBuilder().setName('beeb').setDescription('Replds with pong!'),
+]
+
+	.map(command => command.toJSON());
+console.log(commands)
+const rest = new REST({ version: '9' }).setToken(token);
+
+rest.put(Routes.applicationCommands(clientId),
+	{ body: commands })
+	.then(() => console.log('Successfully registered application commands.'))
+	.catch(console.error);
